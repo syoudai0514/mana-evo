16bb94530c1b577bcf0b1de6cacd944f20a0c5b1
syoudai0514 <syoudai0514@gmail.com>
2026-08-23 22:13:28 +0900

    feat: route ManaEvo study UI to full Kids Quest runtime

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { GameProvider as LearningProvider } from './kids-quest-study/state/GameContext.jsx'
+import './kids-quest-study/styles/learning.css'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LearningProvider>
+      <App />
+    </LearningProvider>
   </React.StrictMode>
 )
