d79b45c9f76c62c05ea22da8f9a3ede55f9f8a6c
syoudai0514 <syoudai0514@gmail.com>
2026-09-03 00:57:00 +0900

    Load isolated Bundle B presentation styles

--- src/game/GameScreens.jsx
+++ src/game/GameScreens.jsx
@@ -1,8 +1,9 @@
 import './game.css'
+import './bundleB.css'
 
 export { AdventureFlow, StageMap } from './screens/AdventureScreen.jsx'
 export { BattleView } from './screens/BattleScreen.jsx'
 export { CapturePanel } from './screens/CapturePanel.jsx'
 export { MonsterScreen } from './screens/MonsterScreen.jsx'
 export { DexGrid } from './screens/DexScreen.jsx'
-export { EvolutionCelebration } from './screens/EvolutionOverlay.jsx'
+export { EvolutionCelebration, EvolutionPrompt } from './screens/EvolutionOverlay.jsx'
